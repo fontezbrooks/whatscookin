9ba7704e2df4296bccdc103b9059b2ed010f0659
fontezbrooks <fontez.brooks@cinemassive.com>
2022-02-13 18:43:25 -0500

    Recipe api getter added

---
+++ recipe-db/fetchdata.ps1
@@ -0,0 +1,7 @@
+$data = Invoke-RestMethod -Uri "https://api.spoonacular.com/recipes/random?number=1&tags=main course&apiKey=f59785e0cb6241beb58e5c6ce953bf8b&"
+
+$FileName = (Get-Date).tostring("dd-MM-yyyy-hh-mm-ss")
+
+$data | ConvertTo-Json -Depth 100 | Out-File ($FileName + ".json")
+
+
