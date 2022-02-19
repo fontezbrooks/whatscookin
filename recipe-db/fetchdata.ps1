57eafbe34590fba3a3afd83db58dc3524742ca5e
fontezbrooks <fontez.brooks@cinemassive.com>
2022-02-19 16:58:12 -0500

    more recipes

--- recipe-db/fetchdata.ps1
+++ recipe-db/fetchdata.ps1
@@ -1,4 +1,4 @@
-$data = Invoke-RestMethod -Uri "https://api.spoonacular.com/recipes/random?number=1&tags=main course&apiKey=f59785e0cb6241beb58e5c6ce953bf8b&"
+$data = Invoke-RestMethod -Uri "https://api.spoonacular.com/recipes/random?number=3&tags=main course&apiKey=f59785e0cb6241beb58e5c6ce953bf8b&"
 
 $FileName = (Get-Date).tostring("dd-MM-yyyy-hh-mm-ss")
 
